6f71c4507f991e5c52812ba06467f7f9ec65c055
irshad vali <irshadvali@irshads-MacBook-Pro.local>
2024-03-04 21:03:37 +0530

    first

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import './index.css';
+import { Link } from 'react-router-dom';
+
+const Sidebar = () => {
+  const [show, setShow] = useState(true);
+
+  return (
+    <main className={show ? 'space-toggle' : null}>
+      
+      <aside className={`sidebar ${show ? 'show' : null}`}>
+        <nav className='nav'>
+          <div>
+            <Link to='/' className='nav-logo'>
+              <i className={`fas fa-home-alt nav-logo-icon`}></i>
+              <span className='nav-logo-name'>Homepage</span>
+            </Link>
+
+            <div className='nav-list'>
+              <Link to='/dashboard' className='nav-link active'>
+                <i className='fas fa-tachometer-alt nav-link-icon'></i>
+                <span className='nav-link-name'>Dashboard</span>
+              </Link>
+              <Link to='/hotel' className='nav-link'>
+                <i className='fas fa-hotel nav-link-icon'></i>
+                <span className='nav-link-name'>Hotel</span>
+              </Link>
+              <Link to='/gallery' className='nav-link'>
+                <i className='fas fa-image nav-link-icon'></i>
+                <span className='nav-link-name'>Gallery</span>
+              </Link>
+              <Link to='/gallery' className='nav-link'>
+                <i className='fas fa-dollar-sign nav-link-icon'></i>
+                <span className='nav-link-name'>Transaction</span>
+              </Link>
+            </div>
+          </div>
+
+          <Link to='/logout' className='nav-link'>
+            <i className='fas fa-sign-out nav-link-icon'></i>
+            <span className='nav-link-name'>Logout</span>
+          </Link>
+        </nav>
+      </aside>
+<div>
+    <div style={{display:'flex', flexDirection:'row', justifyContent:'space-between'}} >  <h1>Content</h1>
+      {/* <div className={`header ${show ? 'space-toggle' : null}`}> */}
+        <div className='header-toggle' onClick={() => setShow(!show)}>
+          <i className={`fas fa-bars ${show ? 'fa-solid fa-xmark' : null}`}></i>
+        </div>
+       
+    </div> 
+    <p>
+      Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+      The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
+      Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+      The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham. 
+      </p>
+    </div>  
+      {/* </div> */}
+
+    </main>
+  );
+};
+
+export default Sidebar;
